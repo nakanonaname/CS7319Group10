30410c73176fd3c3013f42951d3fa67c78b8c6f0
bohanD <100732033+nakanonaname@users.noreply.github.com>
2025-04-04 17:32:54 -0500

    Update PlayerManager.py

--- PlayerManager.py
+++ PlayerManager.py
@@ -4,9 +4,7 @@ class PlayerManager:
         # Initialize players. 'human' for two players, 'ai' for a solo player. Default is two human players.
         self.player_types = {1: 'human', 2: 'human'}
         # Initialize player tokens. Player 1 is red, Player 2 is yellow.
-        self.player_tokens = {1: 'red', 2: 'yellow'}
+        self.player_tokens = {1: 'player 1', 2: 'player 2'}
 
     def switch_turns(self):
         self.blackboard.current_player = 1 if self.blackboard.current_player == 2 else 2
-
-
